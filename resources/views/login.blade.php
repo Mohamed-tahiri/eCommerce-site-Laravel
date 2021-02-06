ccb8a305780b419ca63add8d89a10f38134a083f
Mohamed-tahiri <mhdtahiri01@gmail.com>
2021-02-06 17:07:16 +0100

    login done

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,16 +3,17 @@
     <div class="container custom-login">
         <div class="row">
             <div class="col-sm-4 col-sm-offset-4">
-                <form>
+                <form action="login" method="POST">
                     <div class="form-group">
+                        @csrf
                         <label>Email Address</label>
-                        <input type="text" class="form-control" placeholder="Taper votre Email Address">
+                        <input type="text" name="email" class="form-control" placeholder="Taper votre Email Address">
                     </div>
                     <div class="form-group">
                         <label>Password</label>
-                        <input type="password" class="form-control" placeholder="Taper votre password">
+                        <input type="password" name="password" class="form-control" placeholder="Taper votre password">
                     </div>
-                    <button type="submit" class="btn btn-default">Submit</button>
+                    <button type="submit" class="btn btn-default">Login</button>
                 </form>
             </div>
         </div>
